c118b5a4efea1e32d8a22b010ef057cb938be2c2
tanyasan <ts@theventury.com>
2018-07-11 10:28:07 +0200

    add POST /todos route and refactor mongoose

--- server/server.js
+++ server/server.js
@@ -1,56 +1,28 @@
-var mongoose = require('mongoose');
+var express = require('express');
+var bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect(
-	'mongodb://localhost:27017/TodoApp',
-	{useNewUrlParser: true}
-);
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
-var Todo = mongoose.model('Todo', {
-	name: {
-		type: String,
-		required: true,
-		minlength: 1,
-		trim: true,
-	},
-	completed: {
-		type: Boolean,
-		default: false,
-	},
-	completedAt: {
-		type: Number,
-		default: null,
-	},
-});
+var app = express();
 
-var newTodo = new Todo({
-	name: 'Eat lunch',
-	completed: true,
-	completedAt: 666,
-});
-//saveDocument(newTodo);
+app.use(bodyParser.json());
 
-var User = mongoose.model('User', {
-	email: {
-		type: String,
-		required: true,
-		trim: true,
-		minlength: 1,
-	},
-});
-
-var newUser = new User({
-	email: 't@t.com',
-});
-//saveDocument(newUser);
-
-function saveDocument(doc) {
-	doc.save().then(
+app.post('/todos', (req, res) => {
+	var todo = new Todo({
+		name: req.body.text,
+	});
+	todo.save().then(
 		doc => {
-			console.log(JSON.stringify(doc, undefined, 2));
+			res.send(doc);
 		},
-		error => {
-			console.log('Unable to save', error);
+		e => {
+			res.status(400).send(e);
 		}
 	);
-}
+});
+
+app.listen(3333, () => {
+	console.log('Started on port 3333');
+});
