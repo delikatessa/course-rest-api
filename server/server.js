e0dba38805151310b5b43601a77d9955241ad518
tanyasan <ts@theventury.com>
2018-07-17 15:16:53 +0200

    laptop migration

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/todo');
-var {User} = require('./models/user');
+const {mongoose} = require('./db/mongoose');
+const {Todo} = require('./models/todo');
+const {User} = require('./models/user');
+
+const {ObjectID} = require('mongodb');
 
 var app = express();
 
@@ -23,6 +25,22 @@ app.get('/todos', (req, res) => {
 	Todo.find()
 		.then(todos => res.send({todos}))
 		.catch(error => res.status(400).send(error));
+});
+
+app.get('/todos/:id', (req, res) => {
+	const id = req.params.id;
+	if (!ObjectID.isValid(id)) {
+		res.status(404).send();
+	} else {
+		Todo.findById(id)
+			.then(todo => {
+				if (!todo) {
+					res.status(400).send();
+				}
+				res.status(200).send(todo);
+			})
+			.catch(error => res.status(400).send());
+	}
 });
 
 app.listen(3333, () => {
