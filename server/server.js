a91e4117984683f1e2105c3388205fc935d818ef
Tatyana sandanova <ts@theventury.com>
2018-07-19 11:20:56 +0200

    Setup app for heroku

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
 
-const {ObjectID} = require('mongodb');
-
-var app = express();
+const app = express();
+const port = process.env.PORT || 3333;
 
 app.use(bodyParser.json());
 
@@ -45,8 +45,8 @@ app.get('/todos/:id', (req, res) => {
 		});
 });
 
-app.listen(3333, () => {
-	console.log('Started on port 3333');
+app.listen(port, () => {
+	console.log(`Started up on port ${port}`);
 });
 
 module.exports = {app};
